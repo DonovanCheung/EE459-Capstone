074229d91ee3776599edc628b6b46ef8f0e1da24
Justin Calderon <jfcalder@usc.edu>
2019-03-26 10:35:30 -0700

    Add files via upload

--- interrupt.c
+++ interrupt.c
@@ -1,12 +1,17 @@
-#include "avr/interrupt.h"
-#include "avr/io.h"
+#include <avr/io.h>
+#include <util/delay.h>
+#include <stdio.h>
+#include <avr/interrupt.h>
  
-//#define FOSC 16000000           // Clock frequency
+#define FOSC 16000000           // Clock frequency
 #define BAUD 9600               // Baud rate used
 #define MYUBRR FOSC/16/BAUD-1   // Value for UBRR0 register
 
+void serial_init();
 
-volatile char receivedByte;	// Stores recieved char
+volatile char buf[74];	// Stores recieved char
+volatile int flag;	// Interrupt flag
+volatile int pos =0;	// LCD position
 
 int main(void){
 	
@@ -16,21 +21,43 @@ int main(void){
 	
 	sei(); // Enable Global Interrupt Enable Flag
 	
+	flag = 0;
+	
+	int num=0;
+	GPS* gps;
 	
 	while(1){
 		
 		
+		if(flag == 1){
+			num = parse(buf , gps);
+			if(num){
+				lcd_moveto(1,0);
+				lcd_stringout(buf); 		
 		
+			}	
+			_delay_ms(250);
+			flag = 0;
+			pos = 0;
+		}
 	}
 }
 
+ISR(USART_RXC_vect)
+    {
+        char ch;
 
-ISR(USART_RXC_vect){
-	char received = UDR0;
-	receivedByte = received; // Fetch the received byte value
-	
-	
-}
+        ch = UDR0;                  // Get the received charater
+		
+		buf[pos] = ch;				// Store in buffer
+		pos++;
+		if(pos ==74){
+			buf[74] = '\0';
+			flag = 1;				// If message complete, set flag
+		}
+    } 
+
+
 
 void serial_init(){
 	// initialize USART (must call this before using it)
