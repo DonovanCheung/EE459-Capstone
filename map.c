71bce9cb4e8a46e65e3e46db10fb0c2515a29d98
DonovanCheung <cheungdonovan@yahoo.com>
2019-04-11 10:34:06 -0700

    Added a calcDistance function

--- map.c
+++ map.c
@@ -1,29 +1,23 @@
- #include <stdio.h>
- #include <math.h>
- #include "map.h"
- 
+#include <stdio.h>
+#include <math.h>
+#include "map.h"
+
 double distanceNext(struct GPS* current, struct Map* map){
-	double x = fabs((current->latitude)-(map->next->latitude));
-	double y = fabs((current->longitude)-(map->next->longitude));
-	
-	return sqrt(pow(x,2)+pow(y,2));
+	return calcDistance(current, map->next);
 }
 double distancePrev(struct GPS* current, struct Map* map){
-	double x = fabs((current->latitude)-(map->prev->latitude));
-	double y = fabs((current->longitude)-(map->prev->longitude));
-	
-	return sqrt(pow(x,2)+pow(y,2));
+	return calcDistance(current, map->prev);
 }
 char* directionTo(struct GPS* current, struct GPS* to){
 	char* dir;
 	if((current->longitude - to->longitude) > 0)
 		dir = "N";
 	else	dir = "S";
-	
+
 	if((current->latitude - to->latitude) > 0)
 		dir = strcat(dir,"E");
 	else	dir = strcat(dir,"W");
-		
+
 	return dir;
 }
 char* nextCheck(struct GPS* current, struct Map* map){
@@ -39,56 +33,69 @@ char* nextCheck(struct GPS* current, struct Map* map){
 	strcat(buf, "degees ");
 	return strcat(buf, str);
 }
- 
-double radDegrees(double rad){return (180/3.141593)*rad;}
 
-void ftoa(float n, char *res, int afterpoint){ 
-    // Extract integer part 
-    int ipart = (int)n; 
-  
-    // Extract floating part 
-    float fpart = n - (float)ipart; 
-  
-    // convert integer part to string 
-    int i = intToStr(ipart, res, 0); 
-  
-    // check for display option after point 
-    if (afterpoint != 0) 
-    { 
-        res[i] = '.';  // add dot 
-  
-        // Get the value of fraction part upto given no. 
-        // of points after dot. The third parameter is needed 
-        // to handle cases like 233.007 
-        fpart = fpart * pow(10, afterpoint); 
-  
-        intToStr((int)fpart, res + i + 1, afterpoint); 
-    } 
-} 
-void reverse(char *str, int len){ 
-    int i=0, j=len-1, temp; 
-    while (i<j) 
-    { 
-        temp = str[i]; 
-        str[i] = str[j]; 
-        str[j] = temp; 
-        i++; j--; 
-    } 
+double radDegrees(double rad){return (180/3.141593)*rad;}
+double degRadians(double deg){return deg*(3.141593/180);}
+
+double calcDistance(struct GPS* current, struct GPS* gps){
+  int R = 6371; // Radius of the Earth
+  double dLat = degRadians(gps->latitudeDegrees - current->latitudeDegrees);
+  double dLon = degRadians(gps->longitudeDegrees - current->longitudeDegrees);
+  double a = sin(dLat/2)*sin(dLat/2) + cos(degRadians(gps->latitudeDegrees)) *
+    cos(degRadians(current->latitudeDegrees)) * sin(dLon/2) *
+    sin(dLon/2);
+  double c = 2 * atan2(sqrt(a), sqrt(1-a));
+  double d = R * c;
+  return d;
 }
-int intToStr(int x, char str[], int d){ 
-    int i = 0; 
-    while (x) 
-    { 
-        str[i++] = (x%10) + '0'; 
-        x = x/10; 
-    } 
-  
-    // If number of digits required is more, then 
-    // add 0s at the beginning 
-    while (i < d) 
-        str[i++] = '0'; 
-  
-    reverse(str, i); 
-    str[i] = '\0'; 
-    return i; 
-} 
+
+void ftoa(float n, char *res, int afterpoint){
+    // Extract integer part
+    int ipart = (int)n;
+
+    // Extract floating part
+    float fpart = n - (float)ipart;
+
+    // convert integer part to string
+    int i = intToStr(ipart, res, 0);
+
+    // check for display option after point
+    if (afterpoint != 0)
+    {
+        res[i] = '.';  // add dot
+
+        // Get the value of fraction part upto given no.
+        // of points after dot. The third parameter is needed
+        // to handle cases like 233.007
+        fpart = fpart * pow(10, afterpoint);
+
+        intToStr((int)fpart, res + i + 1, afterpoint);
+    }
+}
+void reverse(char *str, int len){
+    int i=0, j=len-1, temp;
+    while (i<j)
+    {
+        temp = str[i];
+        str[i] = str[j];
+        str[j] = temp;
+        i++; j--;
+    }
+}
+int intToStr(int x, char str[], int d){
+    int i = 0;
+    while (x)
+    {
+        str[i++] = (x%10) + '0';
+        x = x/10;
+    }
+
+    // If number of digits required is more, then
+    // add 0s at the beginning
+    while (i < d)
+        str[i++] = '0';
+
+    reverse(str, i);
+    str[i] = '\0';
+    return i;
+}
--- map.h
+++ map.h
@@ -8,8 +8,8 @@
 #define points 3
 
 struct Map{
-	
-	struct GPS checkpoint[points]; 
+
+	struct GPS checkpoint[points];
 	uint16_t range;					//Distance person can be from checkpoint
 	uint16_t displacement;			//How far a person actually is from checkpoint
 	struct GPS* prev;
@@ -17,13 +17,14 @@ struct Map{
 };
 
 void reverse(char*, int);
-int intToStr(int, char[], int);	 
+int intToStr(int, char[], int);
 void ftoa(float, char*, int);
 double radDegrees(double rad);
 
 double distanceNext(struct GPS* current, struct Map* map);
-double distancePrev(struct GPS* current,struct Map* map); 
+double distancePrev(struct GPS* current,struct Map* map);
 char* directionTo(struct GPS* current, struct GPS* to);
 char* nextCheck(struct GPS* current, struct Map* map);	//output ex: "30 degrees NW"
 
+double calcDistance(struct GPS* current, struct GPS* gps);
 #endif
