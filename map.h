e74b9d6049f6d47f9442c3ada23fb6396f651f1b
raisaifquat <raisa.ifquat@gmail.com>
2019-04-18 16:29:58 -0700

    somewhat working

--- map.h
+++ map.h
@@ -5,32 +5,29 @@
 #include <stdio.h>
 #include "gps.h"
 
-#define totalPoints 3
 #define Range 5
 
-struct Map{
+struct Point{
+	struct GPS gps;
+	struct Point *prev;
+	struct Point *next;
+};
 
-	struct GPS checkpoint[totalPoints];
-	//uint16_t displacement;			//How far a person actually is from checkpoint
-	struct GPS* prev;
-	struct GPS* next;
+struct Map{
+	int totalPoints;
+	struct Point* head;
+	struct Point* curr;
 	int index;
 };
 
-void init_points(struct Map* map_ptr);
-void reverse(char*, int);
-int intToStr(int, char[], int);
-void ftoa(float, char*, int);
+void init_map(struct Map* map_ptr, float points[][2], int numPoints);
 double radDegrees(double rad);
 
 void updateNext(struct GPS* current, struct Map* map);
-double distanceNext(struct GPS* current, struct Map* map);
-double distancePrev(struct GPS* current,struct Map* map);
+double distanceNext(struct GPS* current, struct Point* pt);
 char* directionTo(struct GPS* current, struct GPS* to);
-char* directionNext(struct GPS* current, struct Map* map);
-char* nextCheck(struct GPS* current, struct Map* map);	//output ex: "30 degrees NW"
-
+char* directionNext(struct GPS* current, struct Point* pt);
 
 double calcDistance(struct GPS* current, struct GPS* gps);
-double distFromMap(struct GPS* current, struct Map* map); 
+
 #endif
